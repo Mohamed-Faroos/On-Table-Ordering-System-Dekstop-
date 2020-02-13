5d14678a39642eb863c6d17e2c8ff961f7e1105a
Mohamed-Faroos <faroos1996@gmail.com>
2020-02-13 17:30:01 +0530

    User Management Testing 02/13

---
+++ test/DB/DBUserTest.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DB;
+
+import java.util.List;
+import main.User;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Faroos
+ */
+public class DBUserTest {
+    
+    public DBUserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addUser method, of class DBUser.
+     */
+    @Test
+    public void testAddUser() {
+        System.out.println("addUser");
+        User ow = new User("U001", "Faroos", "Faroos123", 0,0);
+        DBUser instance = new DBUser();
+        boolean expResult = true;
+        boolean result = instance.addUser(ow);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getUser method, of class DBUser.
+     */
+    @Test
+    public void testGetUser() {
+        System.out.println("getUser");
+        String id = "U001";
+        DBUser instance = new DBUser(); 
+        User result = instance.getUser(id);
+        assertEquals("Admin", result.getUsername());
+        assertEquals("admin123", result.getPassword());
+    }
+
+    /**
+     * Test of getAllUser method, of class DBUser.
+     */
+//    @Test
+//    public void testGetAllUser() {
+//        System.out.println("getAllUser");
+//        DBUser instance = new DBUser();
+//        List<User> expResult = null;
+//        List<User> result = instance.getAllUser();
+//        assertEquals(expResult, result);
+//    }
+
+    /**
+     * Test of updateUser method, of class DBUser.
+     */
+    @Test
+    public void testUpdateUser() {
+        System.out.println("updateUser");
+        User us = new User("U001", "Admin", "admin123", 0, 0);
+        DBUser instance = new DBUser();
+        boolean expResult = true;
+        boolean result = instance.updateUser(us);
+        assertEquals(expResult, result);
+    }
+    
+}
