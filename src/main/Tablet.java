d490cbefdef9d3d0f13bb1d0313e77f2f1a5b0b7
Mohamed-Faroos <faroos1996@gmail.com>
2020-02-19 23:23:44 +0530

    Test Tablet Management Backend 02/19.

---
+++ src/main/Tablet.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Faroos
+ */
+public class Tablet {
+    private String tid;
+    private String password;
+    private int status;
+
+    public Tablet(String tid, String password, int status) {
+        this.tid = tid;
+        this.password = password;
+        this.status = status;
+    }
+
+    public Tablet() {
+        this.tid = "";
+        this.password = "";
+        this.status = 0;
+    }
+
+    /**
+     * @return the tid
+     */
+    public String getTid() {
+        return tid;
+    }
+
+    /**
+     * @param tid the tid to set
+     */
+    public void setTid(String tid) {
+        this.tid = tid;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+    
+    
+    
+}
